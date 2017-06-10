c514364e71cbc1c973e29305d09aa47274c31334
marinovskiy <alexender.marinovskiy@gmail.com>
2017-06-10 15:43:30 +0300

    reports

--- src/AppBundle/Controller/Api/ApiReportsController.php
+++ src/AppBundle/Controller/Api/ApiReportsController.php
@@ -15,6 +15,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
  */
 class ApiReportsController extends Controller
 {
+    //TODO date
     /**
      * @Route("/", name="api_report")
      * @Method("GET")
@@ -23,20 +24,56 @@ class ApiReportsController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
 
-        $accountsIds = $request->query->get('accountId');
+        $searchType = $request->query->get('searchType');
+        if ($searchType) {
+//            if ($searchType != 'user' || $searchType != 'organization') {
+//                return $this->json('Wrong search type', 400);
+//            }
+
+            $accountsIds = array();
+
+            if ($searchType == 'user') {
+                foreach ($this->getUser()->getAccounts() as $account) {
+                    array_push($accountsIds, $account->getId());
+                }
+            } else if ($searchType == 'organization') {
+                $organizationId = $request->query->get('organizationId');
+                if (!$organizationId) {
+                    return $this->json('No organizationId provided', 400);
+                }
+
+                $organization = $em->getRepository(Organization::class)->find($organizationId);
+                if (!$organization) {
+                    return $this->json('Organization not found', 404);
+                }
+
+                $membersIds = array();
+                foreach ($organization->getMembers() as $member) {
+                    array_push($membersIds, $member->getId());
+                }
+                if (!in_array($this->getUser()->getId(), $membersIds)) {
+                    return $this->json('Not a member', 403);
+                }
+
+                foreach ($organization->getAccounts() as $account) {
+                    array_push($accountsIds, $account->getId());
+                }
+            } else {
+                return $this->json('Wrong search type', 400);
+            }
+        } else {
+            $accountsIds = $request->query->get('accountId');
+        }
+
+//        $accounts = $this
+//            ->getDoctrine()
+//            ->getRepository(Account::class)
+//            ->loadAccounts($accountsIds);
         $type = $request->query->get('operationType');
         $categoriesIds = $request->query->get('categoryId');
 
-        $logger = $this->get('logger');
-        $logger->info('LOGS');
-        $logger->info('type' . $type);
-        if ($type) {
-            $logger->info('LOGS');
-        }
-
         $categories = array();
         if ($categoriesIds) {
-            $logger->info('LOGS555');
             foreach ($categoriesIds as $categoryId) {
                 $category = $em->getRepository(Category::class)->find($categoryId);
                 if (!$category) {
@@ -46,30 +83,22 @@ class ApiReportsController extends Controller
             }
         }
 
-//        $accounts = $this
-//            ->getDoctrine()
-//            ->getRepository(Account::class)
-//            ->loadAccounts($accountsIds);
-
         $operations = array();
 
         foreach ($accountsIds as $accountId) {
             $account = $em->getRepository(Account::class)->find($accountId);
             if (!$account) {
                 continue;
-//                return $this->json('Account not found', 404);
             }
 
             if ($account->getUser()) {
                 if ($account->getUser()->getId() != $this->getUser()->getId()) {
                     continue;
-//                    return $this->json('Not owner', 403);
                 }
             } else if ($account->getOrganization()) {
                 $organization = $em->getRepository(Organization::class)->find($account->getOrganization()->getId());
                 if (!$organization) {
                     continue;
-//                    return $this->json('Organization not found', 404);
                 }
 
                 $membersIds = array();
@@ -78,24 +107,19 @@ class ApiReportsController extends Controller
                 }
                 if (!in_array($this->getUser()->getId(), $membersIds)) {
                     continue;
-//                    return $this->json('Not a member', 403);
                 }
             }
 
             foreach ($account->getOperations() as $operation) {
-                $logger->info('LOGS0');
                 if ($type) {
-                    $logger->info('LOGS1');
                     if ($operation->getType() == $type) {
                         array_push($operations, $operation);
                     }
                 } else if ($categories) {
-                    $logger->info('LOGS2');
                     if (in_array($operation->getCategory(), $categories)) {
                         array_push($operations, $operation);
                     }
                 } else {
-                    $logger->info('LOGS3');
                     array_push($operations, $operation);
                 }
             }
