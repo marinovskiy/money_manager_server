52287620fb10f989a3a27eb87b1ab2a58b73dc08
marinovskiy <alexender.marinovskiy@gmail.com>
2017-06-11 21:52:47 +0300

    commit

--- src/AppBundle/Entity/Account.php
+++ src/AppBundle/Entity/Account.php
@@ -338,18 +338,33 @@ class Account implements \JsonSerializable
 
     function jsonSerialize()
     {
-        return [
-            'id' => $this->getId(),
-            'name' => $this->getName(),
-            'description' => $this->getDescription(),
-            'currency' => $this->getCurrency(),
+        if ($this->getUser() != null) {
+            return [
+                'id' => $this->getId(),
+                'name' => $this->getName(),
+                'description' => $this->getDescription(),
+                'currency' => $this->getCurrency(),
 //            'currency' => $this->getCurrency()->getId(),
-            'balance' => $this->getBalance(),
-            'operations' => $this->getOperations(),
-            'organization' => $this->getOrganization()->getId(),
-            'createdAt' => $this->getCreatedAt(),
-            'updatedAt' => $this->getUpdatedAt()
-        ];
+                'balance' => $this->getBalance(),
+                'user' => $this->getUser()->getId(),
+                'operations' => $this->getOperations(),
+                'createdAt' => $this->getCreatedAt(),
+                'updatedAt' => $this->getUpdatedAt()
+            ];
+        } else if ($this->getOrganization() != null) {
+            return [
+                'id' => $this->getId(),
+                'name' => $this->getName(),
+                'description' => $this->getDescription(),
+                'currency' => $this->getCurrency(),
+//            'currency' => $this->getCurrency()->getId(),
+                'balance' => $this->getBalance(),
+                'operations' => $this->getOperations(),
+                'organization' => $this->getOrganization()->getId(),
+                'createdAt' => $this->getCreatedAt(),
+                'updatedAt' => $this->getUpdatedAt()
+            ];
+        }
     }
 
     function __toString()
